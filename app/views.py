dbe1dcbfe60fc4799af95d8792873dc970720b2c
Kamalkhan Artykbayev <kamalsdu@MacBook-Pro-Kamalkhan.local>
2018-05-21 15:09:55 +0600

    asd

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseForbidden
-from models import ExampleModel
+# from models import ExampleModel
 import shutil
 import glob
 import os
